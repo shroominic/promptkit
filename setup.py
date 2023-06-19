8d4cfbd69d40c5e2b6d16e67c651e934dc49759e
Shroominic <db2002dominic@gmail.com>
2023-06-19 19:40:01 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='promptkit',
+    version='0.0.1',
+    description='Lightweight prompt engineering toolkit',
+    author='Shroominic',
+    author_email='pleurae-berets.0u@icloud.com',
+    url='https://github.com/shroominic/promptkit',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
