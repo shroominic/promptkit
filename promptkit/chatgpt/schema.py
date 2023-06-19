c2f218a6168e1ec19b41873eb525f2c99c7ad5a5
Shroominic <db2002dominic@gmail.com>
2023-06-19 19:39:29 +0200

    schema

---
+++ promptkit/chatgpt/schema.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+
+
+class BaseMessage(BaseModel):
+    """ Base message """
+    role: str
+    content: str
+    
+    def __str__(self):
+        return self.content
+    
+    def __repr__(self):
+        return f"{self.role}: {self.content}"
+    
+    def __dict__(self):
+        return {"role": self.role, "content": self.content}
+    
+
+class UserMessage(BaseMessage):
+    """ User message """
+    role: str = "user"
+    content: str
+    
+
+class SystemMessage(BaseMessage):
+    """ System message """
+    role: str = "system"
+    content: str
+    
+    
+class AssistantMessage(BaseMessage):
+    """ Assistant message """
+    role: str = "assistant"
+    content: str
+    
+
+class FunctionMessage(BaseMessage):
+    """ Function message """
+    role: str = "function"
+    content: str
+    name: str
+    function_call: dict
