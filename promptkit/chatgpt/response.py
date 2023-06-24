3221b6fbd5c25ab22e8187e76932f9fc1860c810
Shroominic <db2002dominic@gmail.com>
2023-06-24 16:25:38 +0200

    pydantic rewrite

--- promptkit/chatgpt/response.py
+++ promptkit/chatgpt/response.py
@@ -1,51 +1,58 @@
-import re    
-    
+import re
+from pydantic import BaseModel
+from typing import Dict
+from typing import TYPE_CHECKING
 
-class GPTResponse:
+
+if TYPE_CHECKING:
+    from promptkit.chatgpt import ChatGPTSession as Session
+
+
+class GPTResponse(BaseModel):
     """ Represents a response from the chatgpt api """
-    
-    def __init__(self, session, response: dict):
-        self.response = response
-        self.session = session
-    
+    session: Session
+    response: Dict
+
+    @property
+    def message(self) -> str:
+        return self.response["choices"][0]["message"]
+
+    @property
+    def content(self) -> str:
+        return self.message["content"].strip()
+
+    @property
+    def model(self) -> str:
+        return str(self.response["model"])
+
+    @property
+    def usage(self) -> int:
+        return int(self.response["usage"]["total_tokens"])
+
+    @property
+    def finish_reason(self) -> str:
+        return str(self.response["choices"][0]["finish_reason"])
+
     def get_codeblock(self) -> str:
         return re.search(
-            r"```(\w+)(.*?)```", 
+            r"```(\w+)(.*?)```",
             self.content, re.DOTALL
         ).group(2)
 
     def get_bool(self) -> bool:
         """ Extract boolean from response """
         return self.content.lower().startswith("y")
-    
+
     def regenerate(self):
         """ Regenerate the response """
+        if not self.session.history:
+            raise ValueError("Cannot regenerate first response")
         self.response = self.session.generate(history=self.session.history[:-1])
         self.session.history[-1] = self.response.message
         return self
-    
-    @property
-    def message(self) -> str:
-        return self.response["choices"][0]["message"]
-    
-    @property
-    def content(self) -> str:
-        return self.message["content"].strip()
-    
-    @property
-    def model(self) -> str:
-        return str(self.response["model"])
-    
-    @property
-    def usage(self) -> int:
-        return int(self.response["usage"]["total_tokens"])
-    
-    @property
-    def finish_reason(self) -> str:
-        return str(self.response["choices"][0]["finish_reason"])
-    
+
     def __repr__(self) -> str:
         return f"GPTResponse({self.content})"
-    
+
     def __str__(self) -> str:
         return self.content
