999760f4987342662a1abbf4b8642141085bb3c4
Shroominic <db2002dominic@gmail.com>
2023-06-24 16:25:47 +0200

    pydantic rw

--- promptkit/chatgpt/session.py
+++ promptkit/chatgpt/session.py
@@ -1,24 +1,32 @@
 from typing import List
+from pydantic import BaseModel
 from dotenv import load_dotenv
 from os import getenv
-from .response import GPTResponse
 import openai
+from .response import GPTResponse
+from .schema import BaseMessage
+
 
 load_dotenv()
 openai.api_key = getenv("OPENAI_API_KEY")
 
 
-class ChatGPTSession:
+class ChatGPTSession(BaseModel):
     """ Session with the chatgpt api """
-
+    model: str = "gpt-3.5-turbo"
+    history: List[GPTResponse] = []
+    api_key: str | None = None
+    cost: float = 0
+    
     def __init__(self, model: str = "gpt-3.5-turbo", history: List[GPTResponse] = []):
         self.history = history
         self.model = model
-        self.usage = 0
+        self.api_key 
+        self.cost = 0
     
-    def add_user(self, message: str):
+    def add_user(self, message: BaseMessage):
         """ Add a user message to the session history """
-        msg = {"role": "user", "content": f"{message}"}
+        msg = BaseMessage(**{"role": "user", "content": f"{message}"})
         self.history.append(msg)
     
     def add_system(self, message: str):
@@ -31,25 +39,26 @@ class ChatGPTSession:
         self.add_system(instruction)
         return self.get_response()
     
-    def get_response(self) -> GPTResponse:
+    async def get_response(self) -> GPTResponse:
         """ Get response and add it to the session history """
         if self.history[-1]["role"] == "assistant":
             self.add_system_message("Continue your response...")
-        gptresponse = self.generate()
+        gptresponse = await self.generate()
         self.history.append(gptresponse.message)
         print("\n".join([msg["content"].strip() for msg in self.history]))
         return gptresponse
     
-    def generate(self, history=None, **kwargs) -> GPTResponse:
+    async def generate(self, history=None, **kwargs) -> GPTResponse:
         """ Generate a response using the chatgpt api """
-        completion = openai.ChatCompletion.create(
+        completion = await openai.ChatCompletion.acreate(
             model=self.model,
             messages=history or self.history,
             **kwargs
         )
-        self.usage += completion.usage["total_tokens"]
+        
+        # self.cost += completion.usage["total_tokens"] *  model_cost
 
-        return GPTResponse(self, completion)
+        return GPTResponse(session=self, response=completion)
     
     def __repr__(self) -> str:
-        return f"ChatGPTSession(model={self.model}, cost={self.usage * 0.00003})"
+        return f"ChatGPTSession(model={self.model}, cost={})"
