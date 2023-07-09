5df9c98b60449396cc8359e5f21c848c64436af3
Shroominic <db2002dominic@gmail.com>
2023-07-09 17:32:43 +0200

    fix test

--- promptkit/tools/summarize.py
+++ promptkit/tools/summarize.py
@@ -1,28 +1,19 @@
 import re
-from ..chatgpt import ChatGPTSession
-from ..utils import retry_if_false
+from promptkit.chatgpt import ChatSession
+from promptkit.utils import retry_if_false, from_template
 
 
-@retry_if_false(retries=3, except_text="Failed to summarize text")
-def summarize(text: str) -> str:
+# @retry_if_false(retries=3, except_text="Failed to summarize text")
+async def summarize(text: str) -> str | None:
     """ 
     Summarize text using the openai api 
     """
-    response = ChatGPTSession(model="gpt-4").direct_response(
-        f"""
-        Summarize the following text: 
-        {text}
-        
-        Reply with a codeblock and format it like this:
-        ```
-        SUMMARY_START
-        ...
-        SUMMARY_END
-        ```
-        """)
+    response = await ChatSession(model="gpt-4").direct_response(
+        from_template("summarize", text=text),
+    )
     summary = re.search(
         r"SUMMARY_START(.*?)SUMMARY_END", 
         response.content, 
         re.DOTALL
     ) 
-    return summary.group(1).strip() if summary else ""
+    return summary.group(1).strip() if summary else None
