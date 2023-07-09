9cbde886433e71e7552fb5ec8f3d6aada9862ac3
Shroominic <db2002dominic@gmail.com>
2023-07-09 17:32:30 +0200

    chatgpt session

--- promptkit/chatgpt/session.py
+++ promptkit/chatgpt/session.py
@@ -1,64 +1,59 @@
-from typing import List
+from typing import List, TYPE_CHECKING
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from os import getenv
 import openai
-from .response import GPTResponse
-from .schema import BaseMessage
+from promptkit.config import settings
+from promptkit.chatgpt.schema import (
+    BaseMessage,
+    UserMessage,
+    SystemMessage,
+)
+
+if TYPE_CHECKING:
+    from promptkit.chatgpt.response import AssistantResponse
 
 
 load_dotenv()
 openai.api_key = getenv("OPENAI_API_KEY")
 
 
-class ChatGPTSession(BaseModel):
+class ChatSession(BaseModel):
     """ Session with the chatgpt api """
     model: str = "gpt-3.5-turbo"
-    history: List[GPTResponse] = []
-    api_key: str | None = None
+    history: List[BaseMessage] = []
     cost: float = 0
     
-    def __init__(self, model: str = "gpt-3.5-turbo", history: List[GPTResponse] = []):
-        self.history = history
-        self.model = model
-        self.api_key 
-        self.cost = 0
+    def add_user(self, message: str) -> None:
+        self.history.append(UserMessage(content=message))
     
-    def add_user(self, message: BaseMessage):
-        """ Add a user message to the session history """
-        msg = BaseMessage(**{"role": "user", "content": f"{message}"})
-        self.history.append(msg)
+    def add_system(self, message: str) -> None:
+        self.history.append(SystemMessage(content=message))
     
-    def add_system(self, message: str):
-        """ Add a system message to the session history """
-        msg = {"role": "system", "content": message}
-        self.history.append(msg)
-    
-    def direct_response(self, instruction: str) -> GPTResponse:
+    async def direct_response(self, instruction: str) -> "AssistantResponse":
         """ Add a system message as instruction and generate a response """
         self.add_system(instruction)
-        return self.get_response()
+        return await self.get_response()
     
-    async def get_response(self) -> GPTResponse:
+    async def get_response(self) -> "AssistantResponse":
         """ Get response and add it to the session history """
-        if self.history[-1]["role"] == "assistant":
-            self.add_system_message("Continue your response...")
-        gptresponse = await self.generate()
-        self.history.append(gptresponse.message)
-        print("\n".join([msg["content"].strip() for msg in self.history]))
-        return gptresponse
+        response = await self.generate()
+        self.history.append(response)
+        return response
     
-    async def generate(self, history=None, **kwargs) -> GPTResponse:
+    async def generate(self, **kwargs) -> "AssistantResponse":
         """ Generate a response using the chatgpt api """
+        history = [m.model_dump() for m in self.history]
         completion = await openai.ChatCompletion.acreate(
+            api_key=settings.OPENAI_API_KEY,
             model=self.model,
-            messages=history or self.history,
+            messages=history,
             **kwargs
         )
         
         # self.cost += completion.usage["total_tokens"] *  model_cost
-
-        return GPTResponse(session=self, response=completion)
+        from promptkit.chatgpt.response import AssistantResponse  # workaround for circular import 
+        return AssistantResponse(session=self, response=completion)  # type: ignore
     
     def __repr__(self) -> str:
-        return f"ChatGPTSession(model={self.model}, cost={})"
+        return f"ChatSession({self.model}, len: {len(self.history)})"
